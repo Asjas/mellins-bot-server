57b8639c8a0312249066322be9c5fedaef91886b
A-J Roos <asjas@hey.com>
2021-07-22 10:16:52 +0200

    fix order of code for when the user deletes the bot

--- src/commands/reply.ts
+++ src/commands/reply.ts
@@ -1,11 +1,6 @@
 import { logUserActionsInDb } from "../db/telegram";
 
 export async function botReply(ctx: any, message: string, keyboard = {}) {
-  const { customerId: rsaId, joinedPrivateChannel: userJoinedChannel } = ctx;
-  let { message_id: messageId = "", text: userCommand = "" } = ctx.update?.message;
-  const { firstName, lastName, id: userTelegramId } = ctx.update?.message?.from;
-  const botAnswer = firstName ? `Hi, ${firstName},\n\n${message}` : message;
-
   // Incoming message from a user that just deleted the bot
   // We need to handle this as a special case or else the bot crashes
   if (ctx?.update?.my_chat_member?.new_chat_member?.status === "kicked") {
@@ -15,9 +10,15 @@ export async function botReply(ctx: any, message: string, keyboard = {}) {
   // Incoming message from a user that restarted the bot they stopped
   // We need to handle this as a special case or else the bot crashes
   if (ctx?.update?.my_chat_member?.old_chat_member?.status === "kicked") {
-    await ctx.reply(`Welcome back ${firstName}. Your account is now active on Mellins i.Bot`);
+    const { firstName, lastName } = ctx.update?.message?.from;
+    await ctx.reply(`Welcome back ${firstName} ${lastName}. Your account is now active on Mellins i.Bot`);
     return;
   }
+
+  const { customerId: rsaId, joinedPrivateChannel: userJoinedChannel } = ctx;
+  let { message_id: messageId = "", text: userCommand = "" } = ctx.update?.message;
+  const { firstName, lastName, id: userTelegramId } = ctx.update?.message?.from;
+  const botAnswer = firstName ? `Hi, ${firstName},\n\n${message}` : message;
 
   // If the user sent us their `Contact` when requesting a callback we
   // need to manually set the user command value
