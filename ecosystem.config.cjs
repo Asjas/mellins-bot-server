0934fc3ac0998fb24ec8517d01039e82db99dcd5
A-J Roos <asjas@hey.com>
2021-07-27 16:34:18 +0200

    update pm2 config to support both the server and ui

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -2,8 +2,19 @@ module.exports = {
   apps: [
     {
       name: "mellins-bot",
-      script: "./dist/index.mjs",
-      max_memory_restart: "2000M",
+      script: "./index.mjs",
+      cwd: "dist",
+      max_memory_restart: "1200M",
+      env: {
+        NODE_ENV: "production",
+      },
+    },
+    {
+      name: "mellins-ui",
+      script: "npm",
+      args: "start",
+      cwd: "ui",
+      max_memory_restart: "1200M",
       env: {
         NODE_ENV: "production",
       },
